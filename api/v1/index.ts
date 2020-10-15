e13d3f1eb81e908310c5fcd95ee0b5938dd1cd37
tamuraryoya <tamuraryoya@team-lab.com>
2020-10-15 16:56:30 +0900

    🚧 ログの出力を追加

--- api/v1/index.ts
+++ api/v1/index.ts
@@ -1,5 +1,7 @@
 import { NowRequest, NowResponse } from '@vercel/node';
 
 export default (req: NowRequest, res: NowResponse): void => {
+  console.log('run serverless function.');
+
   res.status(200).json({ success: true });
 };
