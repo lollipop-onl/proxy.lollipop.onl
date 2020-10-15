bc3b97064f79d01e44446dd7aeac8c035f605c01
tamuraryoya <tamuraryoya@team-lab.com>
2020-10-15 17:26:07 +0900

    ⚡️ サーバレス関数のエンドポイントを修正

--- api/v1/index.ts
+++ api/v1/index.ts
@@ -1,7 +1,5 @@
 import { NowRequest, NowResponse } from '@vercel/node';
 
 export default (req: NowRequest, res: NowResponse): void => {
-  console.log('run serverless function!');
-
-  res.status(200).json({ success: true });
+  res.status(200).json({ message: 'succeed.' });
 };
